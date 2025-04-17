01c11b2511956a0a306ba526bc40337f7d179041
BaselTurkman <Basel.Turkman123@gmail.com>
2025-04-17 19:12:07 +0300

    Show snackbar alerts for add-to-cart success and duplicates

--- src/pages/products/components/ProductItem.jsx
+++ src/pages/products/components/ProductItem.jsx
@@ -5,27 +5,33 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useDispatch } from "../../../Context/CartProvider";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useCart, useDispatch } from "../../../Context/CartProvider";
+import { useSnackbar } from "../../../Context/snackbarProvider";
 
 export default function ProductItem({ product }) {
   const { name, image, price, description } = product;
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const { showSnackbar } = useSnackbar();
+  const { cart } = useCart();
 
   const handleAddProduct = () => {
-    dispatch({
+    const isFound = cart.find((p) => p.id === product.id);
+
+    if (isFound) {
+      showSnackbar("Product Already exists", "info");
+    } else {
+      dispatch({
         type: "add",
-        payload: product
-    })
-  }
+        payload: product,
+      });
+      showSnackbar("Item added to cart!", "success");
+    }
+  };
 
   return (
     <Card sx={{ maxWidth: 345, margin: "auto" }}>
-      <CardMedia
-        sx={{ minHeight: 220 }}
-        image={image}
-        title={name}
-      />
+      <CardMedia sx={{ minHeight: 220 }} image={image} title={name} />
       <CardContent>
         <Typography gutterBottom variant="h6" component="div">
           {name}
@@ -34,9 +40,21 @@ export default function ProductItem({ product }) {
           {description}
         </Typography>
       </CardContent>
-      <CardActions sx={{padding: 2, display: "flex", justifyContent: "space-between"}}>
-        <Typography color="textSecondary" fontWeight="bold" variant="body1">{price}</Typography>
-        <Button startIcon={<ShoppingCartIcon />} size="small" variant="outlined" onClick={handleAddProduct} > Add to cart </Button>
+      <CardActions
+        sx={{ padding: 2, display: "flex", justifyContent: "space-between" }}
+      >
+        <Typography color="textSecondary" fontWeight="bold" variant="body1">
+          {price}
+        </Typography>
+        <Button
+          startIcon={<ShoppingCartIcon />}
+          size="small"
+          variant="outlined"
+          onClick={handleAddProduct}
+        >
+          {" "}
+          Add to cart{" "}
+        </Button>
       </CardActions>
     </Card>
   );
